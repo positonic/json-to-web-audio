6d5492f30a81e90b5ba70e785bc0b6edf74ef166
James <jamespfarrell@gmail.com>
2016-09-19 11:49:42 +0200

    inital commit

---
+++ js/Voice.js
@@ -0,0 +1,45 @@
+const oscPlayer = (audioContext, voiceConfig) => ({
+
+    getOscillatorConfig(oscNumber)
+    {
+        return voiceConfig.oscillators[oscNumber];
+    },
+
+    getOscillator(oscNumber)
+    {
+        //let oscConfig = getOscillatorConfig(oscNumber);
+        let oscConfig = voiceConfig.oscillators[oscNumber];
+
+        let vco = audioContext.createOscillator();
+        vco.type = oscConfig.waveform;
+
+        return vco;
+    },
+
+    start: (vco, time, noteLength, frequency) => {
+
+        vco.frequency.value = frequency;
+
+        vco.start(time);
+        vco.stop(time + noteLength);
+    }
+
+});
+
+const octave = () => ({
+    applyPipeLength: (frequency, pipeLength) => {
+    return frequency / (parseInt(pipeLength, 10) / 8);
+}
+});
+
+const Voice = (audioContext, voiceConfig)  => {
+
+    return Object.assign(
+        {},
+        oscPlayer(audioContext, voiceConfig),
+        octave()
+    )
+}
+
+
+
