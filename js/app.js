fee4aff41df375e7b5df3a350d6d5d2669f0de43
James <jamespfarrell@gmail.com>
2016-09-21 18:29:39 +0200

    Clean up some comments

--- js/app.js
+++ js/app.js
@@ -4,28 +4,25 @@ import { Voice } from './Voice';
 
 const context = new AudioContext();
 
-const nodeVoiceConfig = {
-    audioNodes : [
+const voiceConfig = {
+    oscillators : [
         {
-            type: 'oscillator',
-            props: {
-                type: "sine",
-                pipeLength: 32,
-                gain: 100,
-                filter: 1000,
-                tuning: 0
-            }
+            type: "sine",
+            pipeLength: 32,
+            gain: 100,
+            filter: 1000,
+            tuning: 0
         },
         {
-            type: 'oscillator',
-            props: {
-                type: "sine",
-                pipeLength: 8,
-                gain: 100,
-                filter: 1000,
-                tuning: 700
-            }
-        },
+            type: "sine",
+            pipeLength: 8,
+            gain: 100,
+            filter: 1000,
+            tuning: 700
+        }
+    ],
+    gain: 100,
+    filters: [
         {
             type: 'filter',
             props: {
@@ -67,120 +64,12 @@ const nodeVoiceConfig = {
                 bypass: 0
             }
         }
-    ],
-
-    gain: 100
-
+    ]
 };
 
-let oscillators = nodeVoiceConfig.audioNodes.filter((audioNode) => {
-    return audioNode.type === 'oscillator';
-}).map(osc => (osc.props));
-
-let filters = nodeVoiceConfig.audioNodes.filter((audioNode) => {
-    return audioNode.type === 'filter';
-});
-
-
-const voiceConfig = {
-    oscillators : [
-        oscillators[0],
-        oscillators[1]
-    ],
-    gain: 100,
-    //filter: 1000
-    filters: filters/*,
-     tunaFilters: [
-     {
-     type: 'Chorus',
-     settings: {
-     rate: 1.5,
-     feedback: 0.2,
-     delay: 0.0045,
-     bypass: 0
-     }
-     }
-     ]
-     */
-};
 let voice = Voice(context, voiceConfig);
 
 var masterVca = voice.setupOscillators();
 let filterInput = voice.setUpFilters(voiceConfig.filters, masterVca, context.destination);
 
 voice.playVoice();
-
-
-
-
-/*const octave = () => ({
-    applyPipeLength: (frequency, pipeLength) => {
-        return frequency / (parseInt(pipeLength, 10) / 8);
-    }
-});*/
-
-/*
-
- const vca = (audioContext, voiceConfig) => {
-
- let masterVca = audioContext.createGain();
- masterVca.gain.value = voiceConfig.gain  / 100;
-
- const me = {
- connectInputToVca: (node) => {
- node.connect(masterVca);
- },
- connectVcaToOutput: (node) => {
- vca.connect(node);
- }
- }
- return me
-
- };
- */
-
-/*const audioNodes = (audioContext, voiceConfig) => {
-
- let masterVca = audioContext.createGain();
- masterVca.gain.value = voiceConfig.gain  / 100;
-
- const me = {
-
- createNodes(nodeConfigs)
- {
- let nodes = [];
- let oscillators = nodeConfigs.filter(nodeConfig => {
- return nodeConfig.type == 'oscillator';
- });
-
- oscillators.forEach(() => {
- nodes = createOscillator(nodeConfig);
- });
-
- debugger
-
- /*
- audioNodesConfigs.forEach(nodeConfig => {
- if(nodeConfig.type == 'oscillator')
- {
- nodes.push(me.createOscillator(nodeConfig));
- }
- else if()
-
- })
- * /
-//me.connectVcaToOutput(output);
-return masterVca;
-},
-
-connectInputToVca: (node) => {
-    node.connect(masterVca);
-},
-    connectVcaToOutput: (node) => {
-    vca.connect(node);
-}
-}
-return me
-
-};
-*/
